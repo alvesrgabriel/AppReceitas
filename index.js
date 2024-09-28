9f60cfb1aef8a63d8097712506b5f3008dff7e62
alvesrgabriel <gabriel.luky1@gmail.com>
2024-09-27 21:39:26 -0300

    adicionando-src

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express")//para usar o express
 const server = express ()//para criar um servidor
+const alunos = require("./src/teste.json")
 
 server.get("/", (req, res)=>{
     return res.json({mensagem:"Hello NODE"})
 })
 
 server.get("/msg", (req, res)=>{
-    return res.json({mensagem:"Olá!"})
+    return res.json({mensagem:"Olá!!"})
+})
+
+server.get("/alunos", (req, res)=>{
+    return res.json(alunos)
 })
 
 server.listen(3300, ()=>{
     console.log('server ON')
 })
+
